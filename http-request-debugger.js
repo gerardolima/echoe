6f23456c41f22b3bd4f028331b8ada08f208cb61
Lima, Gerardo <gerardo.lima@pmi.com>
2022-03-25 10:44:33 +0000

    feat: allow url params to controll logged info

--- http-request-debugger.js
+++ http-request-debugger.js
@@ -3,6 +3,7 @@
 // https://nodejs.org/docs/latest-v14.x/api/http.html#http_event_request
 
 const http = require('http')
+const { URL } = require('url')
 
 const port = process.env['PORT'] ?? 3000
 
@@ -14,15 +15,34 @@ const log = (v, e='binary') => Buffer.isBuffer(v)
   ? console.log(v.toString(e))
   : console.log(v)
 
+/** @param {string} url */
+const parseParams = (url) => {
+  const {searchParams} = new URL(`http://host${url}`)
+  const falsy = ['n', 'no', '0', 'false']
+
+  const showUrl = !falsy.includes(searchParams.get('u'))
+  const showHeaders = !falsy.includes(searchParams.get('h'))
+  const showBody = !falsy.includes(searchParams.get('b'))
+
+  /** @type {TranscodeEncoding} */
+  let encoding = searchParams.get('e')
+  if(!Buffer.isEncoding(encoding)) encoding = 'binary'
+
+  return {showUrl, showHeaders, showBody, encoding}
+}
+
 /** @type http.RequestListener */
 const requestListener = function (req, res) {
-  const {headers} = req
+  const {headers, method, url} = req
+
+  const {showUrl, showHeaders, showBody, encoding} = parseParams(url)
+
+
+  if(showUrl) log(`[${(new Date).toISOString()}] ${method}: ${url}`)
+  if(showHeaders) log(headers)
+  if(showBody) req.on('data', buf => log(buf, encoding))
+
   res.statusCode = 204
-
-  log(`${req.method}: ${req.url}`)
-  log(headers)
-
-  req.on('data', buf => log(buf, 'binary'))
   res.end()
 }
 
